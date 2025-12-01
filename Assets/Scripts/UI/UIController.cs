c273810ee345f76332ccb7e5b7330fb2bb383670
Joa Lamminluoto <johannes101001@gmail.com>
2025-12-01 15:20:36 +0200

    pause menu added

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,13 +1,52 @@
 ﻿using UnityEngine;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    [Header("Dice Reference")]
     public Dice dice;
 
-    // Called by the Roll Dice button
+    [Header("Pause Menu UI")]
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // ESC opens and closes pause menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    // --- DICE ROLL BUTTON ---
     public void OnRollDice()
     {
         GameManager.Instance.PlayerRoll();
+    }
+
+    // --- PAUSE MENU FUNCTIONS ---
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
